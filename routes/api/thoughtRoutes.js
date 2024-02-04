3fc9da4771b530c03e38f0b1afa6db9f94e51d1d
Ruminate <inramiland@gmail.com>
2024-02-04 13:13:14 -0500

    removed commented out code, unneeded

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,22 +1,3 @@
-// const router = require('express').Router();
-// const thoughtController = require('../../controllers/thought-controller');
-
-// const {
-//     getThoughts,
-//     getSingleThought,
-//     createThought,
-//     updateThought,
-//     deleteThought,
-//     addReaction,
-//     deleteReaction
-// } = require('../../controllers/thought-controller');
-
-// router.route('/').get(thoughtController.getThoughts).post(thoughtController.createThought);
-// router.route('/:thoughtId').get(thoughtController.getSingleThought).put(thoughtController.updateThought).delete(thoughtController.deleteThought);
-// router.route('/:thoughtId/reactions').post(thoughtController.addReaction);
-// router.route('/:thoughtId/reactions/:reactionId').delete(thoughtController.deleteReaction);
-
-// module.exports = router;
 const router = require('express').Router();
 const thoughtController = require('../../controllers/thought-controller');
 
